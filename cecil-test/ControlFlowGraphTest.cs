1db6ad2b8aefff14bd4e73156f560526bd9198d4
arisKoutsou <sdi1600078@di.uoa.gr>
2020-03-03 16:34:42 +0200

    Added cecil test

---
+++ cecil-test/ControlFlowGraphTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+class ControlFlowGraphTest
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Starting the testing sequence...");
+        Console.WriteLine("Current directory is: " + Directory.GetCurrentDirectory());
+        Console.WriteLine();
+        ModuleDefinition module = ModuleDefinition.ReadModule("BubbleSort.exe");
+        CilControlFlowGraph cfg = new CilControlFlowGraph(module, "MySort", "Main");
+        Console.WriteLine(cfg);
+    }
+
+    static void PrintSomeInstructionsThatTransferControl() {
+        foreach (var p in typeof(OpCodes).GetFields())
+        {
+            var v = (OpCode)p.GetValue(null); // static classes cannot be instanced, so use null...                
+            if (v.OperandType == OperandType.InlineBrTarget || 
+                v.OperandType == OperandType.ShortInlineBrTarget) {
+                Console.WriteLine(v.Name);
+            }
+        }
+    }
+}
