adc7e675bad75ccb5a4ecfa4fa166c497ac12ede
arisKoutsou <sdi1600078@di.uoa.gr>
2020-03-23 17:00:40 +0200

    added csproj file

--- sample-cil/disass.sh
+++ sample-cil/disass.sh
@@ -3,6 +3,11 @@
 # Compile a single file containing a C# Main entry point.
 # Generate the '.exe' file as well as the '.il' file.
 
+if [[ "$#" -lt 1 ]]; then
+    echo "Provide a C# entry point class"
+    exit 1
+fi
+
 mcs $1
 NAME="${1%%.*}"
 monodis "$NAME.exe" > "$NAME.il"
